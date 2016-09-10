4110929d2af88678ae25e3631f4093e496e1312c
jalatraining <jalatraining@fundacion-jala.local>
2016-09-10 11:50:12 -0400

    Adding max and min functions

--- tasks/arguments/bolivia.js
+++ tasks/arguments/bolivia.js
@@ -16,7 +16,50 @@ function sum(ns) {
 }
 
 //Calculate average
-
 function average() {
     return calculateSum(arguments, arguments.length)/arguments.length;
+}
+
+//Calculate max value
+var calculateMax = function (numbers, pos, max) {
+    if(pos == 1) {
+        if (numbers[0] > max) {
+            return numbers[0];
+        }
+        else {
+            return max;
+        }
+    } else {
+        if (numbers[pos - 1] > max) {
+            return calculateMax(numbers, pos - 1, numbers[pos - 1]);
+        } else {
+            return calculateMax(numbers, pos - 1, max);
+        }
+    }
+};
+
+function max() {
+    return calculateMax(arguments, arguments.length, arguments[arguments.length - 1])
+}
+
+//Calculate min
+var calculateMin = function (numbers, pos, min) {
+    if(pos == 1) {
+        if (numbers[0] < min) {
+            return numbers[0];
+        }
+        else {
+            return min;
+        }
+    } else {
+        if (numbers[pos - 1] < min) {
+            return calculateMin(numbers, pos - 1, numbers[pos - 1]);
+        } else {
+            return calculateMin(numbers, pos - 1, min);
+        }
+    }
+};
+
+function min() {
+    return calculateMin(arguments, arguments.length, arguments[arguments.length - 1])
 }
