fde62e833675a061769bccfca461262428a4513a
jalatraining <jalatraining@fundacion-jala.local>
2016-09-10 11:34:12 -0400

    Adding average calculation

--- tasks/arguments/bolivia.js
+++ tasks/arguments/bolivia.js
@@ -2,8 +2,7 @@
  * Created by jalatraining on 9/10/2016.
  */
 
-// Max number
-
+// Calculate total sum
 var calculateSum = function (numbers, pos) {
     if(pos == 1) {
         return numbers[0];
@@ -16,3 +15,8 @@ function sum(ns) {
     return calculateSum(arguments, arguments.length);
 }
 
+//Calculate average
+
+function average() {
+    return calculateSum(arguments, arguments.length)/arguments.length;
+}
