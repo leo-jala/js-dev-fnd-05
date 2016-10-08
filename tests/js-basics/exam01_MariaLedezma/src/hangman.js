0f46d21d951131a639de05c8c6f0b7a4bdbc4f77
MariaL <maria.ledezma@jalasoft.com>
2016-10-08 11:00:31 -0400

    Examen1 - JS Basics - Maria L.

---
+++ tests/js-basics/exam01_MariaLedezma/src/hangman.js
@@ -0,0 +1,115 @@
+var Menu = function (){
+	this.hangman;
+	this.words = [];
+};
+
+Menu.prototype.displayOptions = function(){
+	var textMenu = "1. Feed Collection of Words <br> 2. Play <br> 3. Quit <br>";
+	document.getElementById('menu').visibility = "visible";
+    document.getElementById('menu').innerHTML = textMenu;
+};
+
+Menu.prototype.controlOption = function(option){
+
+    switch(option) {
+        case 1:
+               var words = "hola,test,mundo,definicion,adivinanza";
+			   this.feedWords(words)
+			   document.getElementById("msg").innerHTML="Your WORDS were feeded!!!";
+            break;
+        case 2:
+			this.hideMenu();
+            if(this.words.length==0){
+				var words = "hola,test,mundo,definicion,adivinanza";
+				this.feedWords(words)
+			}
+			word = this.getRandomWord();
+			this.hangman = new Hangman(word);
+			this.hangman.displayGame();			
+            break;
+        default:
+            document.getElementById("msg").innerHTML="GAME OVER!!!";
+			//window.close();
+	}
+};
+
+Menu.prototype.hideMenu = function(){
+	document.getElementById('menu').innerHTML= "";
+};
+
+Menu.prototype.feedWords = function(words){
+	this.words = words.split(',');
+};
+
+Menu.prototype.getRandomWord = function(){
+	var index = getRandom(this.words.length-1);
+	return this.words[index];
+};
+
+
+function getRandom(limit) {
+    return Math.floor( Math.random() * ( 1 + limit - 0 ) );
+};
+
+
+
+var CellLetter = function (letter){
+	this.value = letter;
+	this.match = false;
+   
+};
+/**
+* Hangman class
+* @params {string} word - the word to be used for guessing
+*/ 
+var Hangman = function (word){
+	this.arrLetters = new Array();
+	this.setCellLetters(word.split(''));
+   
+};
+Hangman.prototype.setCellLetters = function(letters){
+	for(i = 0; i < letters.length; i++){
+		this.arrLetters[i] = new CellLetter(letters[i]);
+	}
+}
+
+Hangman.prototype.controlLetter = function(letter){
+	var valid = false;
+	for(i = 0; i < this.arrLetters.length; i++){
+		console.log(letter + "  " + this.arrLetters[i].value)
+		if(this.arrLetters[i].value==letter){
+			this.arrLetters[i].match= true;
+		}
+	}
+};
+
+Hangman.prototype.displayGame = function(){
+	var contentGame = "";
+	for(i = 0; i < this.arrLetters.length; i++){
+		if(this.arrLetters[i].match){
+			contentGame += this.arrLetters[i].value + "  ";
+		}
+		else{
+			 contentGame += "*  "
+		}
+	}
+	document.getElementById("content").innerHTML=contentGame;
+	this.controlSolution();
+};
+
+Hangman.prototype.isSolved = function(){
+	var valid = true;
+	for(i = 0; i < this.arrLetters.length; i++){
+		if(!this.arrLetters[i].match){
+			valid = false;
+			break;
+		}
+	}
+	return valid;
+};
+Hangman.prototype.controlSolution = function(){
+	if(this.isSolved()){
+		document.getElementById("msg").innerHTML="YOU WON.......GAME OVER!!!";
+	}
+	
+};
---
+++ tests/js-basics/exam01_MariaLedezma/src/main.js
@@ -0,0 +1,32 @@
+var menu;
+var hangman;
+
+/**
+* Create the instance of Hangman Game based on the values given in the HTML page
+*/
+
+
+var displayMenu = function(){
+	menu = new Menu();
+	menu.displayOptions();
+	document.getElementById("msg").innerHTML="";
+};
+var optionMenu = function(){
+	var option = document.getElementById('opt_input').value;
+	if(!isNaN(option) && parseInt(option)> 0 &&  parseInt(option)< 4){
+		document.getElementById("msg").innerHTML="";
+		menu.controlOption(parseInt(option));
+		
+	}
+	else{
+		document.getElementById("msg").innerHTML="INVALID OPTION!!!";
+	}
+}
+
+var play = function(){
+	
+	var letter = document.getElementById('letter_input').value;
+	hangman = menu.hangman;
+	hangman.controlLetter(letter);
+	hangman.displayGame();
+}
