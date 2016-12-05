1efcfd2ed8f2bfa286ad8c5ee22ab3e683b442e2
MariaL <maria.ledezma@jalasoft.com>
2016-12-04 23:31:11 -0400

    Final Exam API - Mledezma, second question

--- tests/js-api-testing/MLedezma/todoly/projectSpecs/projectsSpec.js
+++ tests/js-api-testing/MLedezma/todoly/projectSpecs/projectsSpec.js
@@ -4,12 +4,14 @@ var faker = require('faker');
 
 var TodoLy = require('./../../framework/todoLy');
 var Projects = TodoLy.resources.projects;
+var Items = TodoLy.resources.items;
 
 describe('Todo.ly', function(){
 	describe('Projects', function(){
 		
 		var project;
 		var newProject;
+		var newItemData1;
 		
 		beforeEach(function(done){
 			project = {
@@ -17,7 +19,14 @@ describe('Todo.ly', function(){
 			};
 			Projects.create(project, function(err, project){
 				newProject = project;
-				done();				
+
+				newItemData1 = {
+				Content: faker.name.title(),
+    			ProjectId: newProject.Id,
+				};
+				Items.create(newItemData1, function(err, newItemData1){
+					done();	
+				});			
 			});
 		});
 		afterEach(function(){
@@ -26,7 +35,24 @@ describe('Todo.ly', function(){
 		});
 		
 		it('Create project child and item child', function(done){
-			//TODO
+			var childProject = {
+				ParentId: newProject.Id,
+				Content: faker.name.title()
+			};
+			Projects.create(childProject, function(err, childProject){
+				expect(newProject.Id).toEqual(childProject.Id);
+				Projects.get(newProject.Id, function(err, project){
+					expect(project.Children).toContain(childProject);
+					var newChildItemData = {
+						Content: faker.name.title(),
+		    			ProjectId: childProject.Id,
+						};
+					Items.create(newChildItemData, function(err, newChildItemData){
+						expect(newChildItemData.ProjectId).toEqual(childProject.Id);
+						done();	
+					});	
+				});
+			});
 		});
 	});	
 });
